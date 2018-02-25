c98f61216ab3e2b0f336185adc2eada3ee924c5d
joonakeskitalo <asd>
2018-02-25 22:47:10 +0200

    Labs 5 and 6

--- docs/js/script.js
+++ docs/js/script.js
@@ -5,8 +5,38 @@ function addName() {
   list.innerHTML += "<br>" + name;
 }
 
-// Lab 3.3 Audio player
 
+// Lab 3.1 WebSocket Chat
+
+var connection = new WebSocket("ws://obscure-waters-98157.herokuapp.com");
+
+// Log errors
+connection.onerror = function(error) {
+  console.log('WebSocket Error ' + error);
+};
+
+// Log messages from the server
+connection.onmessage = function(e) {
+  console.log('Server: ' + e.data);
+};
+
+function sendMessage() {
+  var myform = document.getElementById("chatForm").value;
+  // console.log(myform);
+  connection.send(myform);
+}
+
+window.console = {
+  log: function(str) {
+    var node = document.createElement("div");
+    node.appendChild(document.createTextNode(str));
+    document.getElementById("myLog").appendChild(node);
+  }
+}
+
+
+
+// Lab 3.3 Audio player
 // https://stackoverflow.com/questions/27368778/how-to-toggle-audio-play-pause-with-one-button-or-link
 // https://stackoverflow.com/questions/32705989/audio-range-volume-slider-javascript-html5
 var audio = document.getElementById("audio");
@@ -17,8 +47,8 @@ function togglePlay() {
 
 function setVolumeAudio() {
   audio.volume = document.getElementById("volume1").value;
-
 }
+
 
 // Lab 3.4 Video player
 var video = document.getElementById("video");
@@ -32,56 +62,68 @@ function setVolumeVideo() {
 }
 
 
+// Lab 4.1
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext('2d');
+var pos = { x: 0, y: 0 };
+canvas.addEventListener('mousedown', setStart);
+canvas.addEventListener('mousemove', draw);
+canvas.addEventListener('mouseup', setEnd);
+
+function getCoords(e) {
+  var rect = canvas.getBoundingClientRect();
+  pos.x = e.clientX - rect.left;
+  pos.y = e.clientY - rect.top;
+}
+
+function setStart(e) {
+  getCoords(e);
+  ctx.moveTo(pos.x, pos.y);
+  ctx.beginPath();
+}
+
+function setEnd(e) {
+    ctx.closePath();
+  }
+
+  function draw(e) {
+    if (e.buttons !== 1) return
+      ctx.lineTo(pos.x, pos.y);
+    getCoords(e);
+    ctx.stroke();
+  }
 
 
+// Lab 5.3
+function jsonCallback(json) {
+  $.each(json, function(index, value) {
+    console.log(value.name, value.email);
+    document.getElementById("jsonContent").innerHTML +=
+    "<a href='mailto:" + value.email + "'>" + value.name + "</a>" + "<br>";
+  });
+}
+
+function parseJson() {
+  $.ajax({
+    url: "https://imp-portfolio-demonstration.herokuapp.com/json/persons.jsonp",
+    dataType: "jsonp",
+  });
+}
 
 
-
-
-
-
-
-
-
-// Lab 4.1 Canvas Drawing application
-
-// var canvas = document.getElementById('tutorial');
-// var ctx = canvas.getContext('2d');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Lab 6.0
+$("#search").click(function() {
+  document.getElementById("flickrResults").innerHTML = "";
+  $("#results").empty();
+  $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?", {
+    tags: $("#flickrSearch").val(),
+    tagmode: "any",
+    format: "json"
+  },
+  function(data) {
+    $.each(data.items, function(i, item) {
+      $("<img>").attr("src", item.media.m).appendTo("#flickrResults");
+      if (i == 30) return false;
+    });
+  });
+});
