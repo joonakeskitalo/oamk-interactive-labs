1dcf50bad1ea9507dc597414ef9caf68326edb73
Joona Keskitalo <35720395+joonakeskitalo@users.noreply.github.com>
2018-02-26 10:28:53 +0200

    Updated navigation

--- nav.js
+++ nav.js
@@ -2,6 +2,7 @@ document.write('<div class="nav">\
   <a class="navTitle" href="index.html" title=""><h1>Portfolio</h1></a>\
   <a class="navLinks" href="index.html" title="">Home</a>\
   <a class="navLinks" href="labs.html" title="">Labs</a>\
-  <a class="navLinks" href="https://github.com/joonakeskitalo?tab=repositories" title="">Github</a>\
+  <a class="navLinks" href="https://github.com/joonakeskitalo/oamk-interactive-labs" title="">Github</a>\
   </div>\
   ');
+
